42db9e1c77a2e5d389fde6ae34b8577865b497fc
Jan Rock <rock@defihq.org>
2022-10-19 18:24:57 +0100

    Initial Commit

--- src/main/java/org/yottalabs/twoSum.java
+++ src/main/java/org/yottalabs/twoSum.java
@@ -1,7 +1,27 @@
 package org.yottalabs;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] out = new int[0];
+        int y = 0;
+        while (out.length <= 1) {
+            for (int i = 0; i + 1 + y < nums.length; i++) {
+                if (target == nums[i] + nums[i + 1 + y]) {
+                    out = new int[]{i, i + 1 + y};
+                    break;
+                }
+            }
+            y++;
+        }
+        return out;
     }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3, 1, 5};
+        int target = 8;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
 }
